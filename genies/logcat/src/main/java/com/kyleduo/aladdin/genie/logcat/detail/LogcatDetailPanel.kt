9eb2434734d43b16e725ca474a2117742abf42d8
kyleduo <kyleduo@gmail.com>
2021-06-23 13:33:34 +0800

    optimise log detail style

--- genies/logcat/src/main/java/com/kyleduo/aladdin/genie/logcat/detail/LogcatDetailPanel.kt
+++ genies/logcat/src/main/java/com/kyleduo/aladdin/genie/logcat/detail/LogcatDetailPanel.kt
@@ -1,6 +1,10 @@
 package com.kyleduo.aladdin.genie.logcat.detail
 
 import android.content.res.ColorStateList
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.RelativeSizeSpan
 import android.view.ViewGroup
 import android.widget.TextView
 import com.kyleduo.aladdin.genie.logcat.data.LogItem
@@ -9,6 +13,7 @@ import com.kyleduo.aladdin.genie.logcat.view.LogItemStyles
 import com.kyleduo.aladdin.ui.UIUtils
 import com.kyleduo.aladdin.ui.dp2px
 import com.kyleduo.aladdin.ui.layoutInflater
+
 
 /**
  * @author kyleduo on 2021/6/23
@@ -35,7 +40,17 @@ class LogcatDetailPanel(
         (root.parent as? ViewGroup)?.removeView(root)
 
         container.addView(root)
-        binding.aladdinLogcatLogDetailContent.text = item.content
+
+        val content: String = item.content.replace("\n", "\n\n")
+        val ss: Spannable = SpannableString(content)
+        for (i in 1 until content.length - 1) {
+            if (content[i] == '\n' && content[i - 1] == '\n') {
+                ss.setSpan(
+                    RelativeSizeSpan(0.5f), i, i + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+        }
+        binding.aladdinLogcatLogDetailContent.setText(ss, TextView.BufferType.SPANNABLE)
 
         val itemStyle = logItemStyles.getStyle(item.level)
 
